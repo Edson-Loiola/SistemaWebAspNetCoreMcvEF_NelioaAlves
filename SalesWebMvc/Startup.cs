13e1702cad5d72283b9a29c90fa4d0c8bc46d7f4
Edson <edson.monteiro.l@gmail.com>
2020-08-09 00:14:02 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -29,6 +29,7 @@ namespace SalesWebMvc
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -36,8 +37,10 @@ namespace SalesWebMvc
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
